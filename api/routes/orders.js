7e6f1212f46f28cff69c4426a044e883963c3dba
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-14 17:43:32 +0530

    add functions to post, get, delete orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,40 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const Order = require('../models/orders')
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched'
-  })
+  Order.find().populate('product')
+    .exec()
+    .then((docs) => { res.status(200).json(docs) })
+    .catch((error) => { res.status(500).json(error) })
 })
 
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  }
-  res.status(201).json({
-    message: 'Orders was created',
-    order: order
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId
   })
+  order.save()
+    .then((result) => { res.status(201).json(result) })
+    .catch((error) => { res.status(500).json(error) })
 })
 
-router.get('/:orderid', (req, res, next) => {
-  res.status(200).json({
-    message: 'orders details',
-    orderid: req.params.orderid
-  })
+router.get('/:orderId', (req, res, next) => {
+  Order.findById(req.params.orderId).populate('product').exec()
+    .then((order) => {
+      res.status(200).json(order)
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
 })
 
-router.delete('/:orderid', (req, res, next) => {
-  res.status(200).json({
-    message: 'orders details',
-    orderid: req.params.orderid
-  })
+router.delete('/:orderId', (req, res, next) => {
+  Order.remove({ _id: req.params.orderId }).exec()
+    .then((result) => { res.status(200).json(result) })
+    .catch((err) => { res.status(500).json(err) })
 })
 
 module.exports = router
